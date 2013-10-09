c6845f4c6d38dac474560b2a0267c1700f9f2ca5
yutaro <y.tanaka.kcbbf@gmail.com>
2013-10-10 00:29:14 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * DOM読み込み時に実行される
+ */
+$().ready(function () {
+
+// 楽天ブックス書籍検索API
+    var url = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?";
+    var appId = "applicationId=" + 1025628587333296296;
+    var title = "&title=%E5%A4%AA%E9%99%BD";
+    var genreId = "&booksGenreId=001004008";
+    var sort = "&sort=%2BitemPrice";
+});
