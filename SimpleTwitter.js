4cbdb1f9d816daa841ff55771f8d4c5c417d105d
yutaro <y.tanaka.kcbbf@gmail.com>
2013-10-20 23:51:05 +0900

    定数の代わりに関数使用を試した

--- SimpleTwitter.js
+++ SimpleTwitter.js
@@ -1,10 +1,11 @@
 if (Meteor.isClient) {
-    // ②テンプレート関数の属性として定義
-    Template.Header.dateTime = new Date;
-    //Template.PlayerList.name = "Yutaro"; この書き方はだめ「name」
-    Template.PlayerList.playerName = "Yutaro";
+    Template.Header.dateTime = function() {
+        return new Date;
+    }
+    Template.PlayerList.playerName = function() {
+        return "Yutaro";
+    }
 }
-
 if (Meteor.isServer) {
     // プロセスが開始したら呼び出される
     Meteor.startup(function() {
