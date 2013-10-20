42ec4edab02917774c32a223fcc953442dee901d
yutaro <y.tanaka.kcbbf@gmail.com>
2013-10-21 00:13:35 +0900

    Meteor.methodsは本来サーバ側のリモートメソッドだと気づく

--- SimpleTwitter.js
+++ SimpleTwitter.js
@@ -1,11 +1,24 @@
-if (Meteor.isClient) {
-    Template.Header.dateTime = function() {
-        return new Date;
-    }
-    Template.PlayerList.playerName = function() {
-        return "Yutaro";
-    }
+var getPlayerName = function(firstName, lastName) {
+        this.unblock();
+        if (this.is_simulation) {
+            // クライアントスタブ上での処理
+            // 並列実行を許可する
+            return firstName + " " + lastName;
+        } else {
+            // サーバ上で実行された際の処理
+        }
 }
+
+if (Meteor.isClient) {
+        Template.Header.dateTime = function() {
+            return new Date;
+        }
+        Template.PlayerList.playerName = function() {
+  //            return "Yutaro";
+            return new getPlayerName('Yutaro', 'Tanaka');
+        }
+}
+
 if (Meteor.isServer) {
     // プロセスが開始したら呼び出される
     Meteor.startup(function() {
@@ -13,9 +26,17 @@ if (Meteor.isServer) {
             console.log("クライアント初期化！");
         } else {
             console.log("サーバ初期化！");
+            // サーバ側をいじるためのメソッド！！！！！！！
             Meteor.methods({
                 getPlayerName: function(firstName, lastName) {
-                    return firstName + " " + lastName;
+                    this.unblock();
+                    if (this.is_simulation) {
+                        // クライアントスタブ上での処理
+                        // 並列実行を許可する
+                        return firstName + " " + lastName;
+                    } else {
+                        // サーバ上で実行された際の処理
+                    }
                 }
             });
         }
