16e2bbaab3887a7b3736e34cff8b77eb64d9a5db
yutaro <y.tanaka.kcbbf@gmail.com>
2014-01-20 08:22:46 +0900

    Add TimerController.

---
+++ src/main/js/service/TimerService.js
@@ -0,0 +1,9 @@
+function TimerService() {
+}
+
+//TimerService.prototype.intValidation = function (num) {
+//    if (num.match(/[^0-60]/g) || (parseInt(num, 10) + "" != num)) {
+//        num = 0;
+//    }
+//    return num;
+//};
