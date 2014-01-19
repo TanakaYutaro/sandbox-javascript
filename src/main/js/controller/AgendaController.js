76b8a75e70f8e51e481664f207aff7c50c2de056
yutaro <y.tanaka.kcbbf@gmail.com>
2014-01-20 08:22:21 +0900

    Can Delete Agendas.

--- src/main/js/controller/AgendaController.js
+++ src/main/js/controller/AgendaController.js
@@ -1,7 +1,5 @@
 function AgendaController($scope) {
     $scope.agendas = [
-        "hoge",
-        "huga"
     ];
 
 
@@ -10,5 +8,9 @@ function AgendaController($scope) {
             $scope.agendas.push($scope.newAgenda);
             $scope.newAgenda = "";
         }
+    };
+
+    $scope.deleteAgenda = function(index) {
+        $scope.agendas.splice(index, 1);
     };
 }
