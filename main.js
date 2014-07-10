541fc0490fd0f8538c5ea606ae71084f0917a8e9
yutaro <y.tanaka.kcbbf@gmail.com>
2014-07-11 01:14:59 +0900

    Mod for Blog.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 $().ready(function () {
     var panel = $('.panel');
-    panel.bind('touchmove', function (evt) {
-        var touch = evt.originalEvent.touches[0];
-        var target = $(document.elementFromPoint(touch.clientX, touch.clientY));
+    panel.on({
+        'mouseenter': function () {
+            var target = $(this);
+            if (target.hasClass('panel')) {
+                target.addClass('green');
+            }
+        },
+        'touchmove': function (event) {
+            var touch = event.originalEvent.touches[0];
+            var target = $(document.elementFromPoint(touch.clientX, touch.clientY));
 
-        if (target.hasClass('panel')) {
-            panel.removeClass('green');
-            target.addClass('green');
+            if (target.hasClass('panel')) {
+                target.addClass('green');
+            }
         }
+    });
+
+    panel.each(function (i, val) {
+        $(this).text(i + 1);
     });
 });
